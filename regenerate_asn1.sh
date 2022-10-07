31c94f50412a70d8596d859e114e013d9904406a
Markus Heberling <markus@heberling.net>
2022-10-07 09:11:20 +0300

    added v2.1 API

--- regenerate_asn1.sh
+++ regenerate_asn1.sh
@@ -19,3 +19,21 @@ asn1extractor/asn1extractor.sh ApplicationDataModule \
   com.saicmotor.telematics.tsgp.otaadapter.mp.v1_1.entity.software.MPAppAttributeResp \
   com.saicmotor.telematics.tsgp.otaadapter.mp.v1_2.entity.advertise.AdvertiseResp \
            > "ASN.1 schema/v1_1/ApplicationData.asn1"
+
+rm -rf "ASN.1 schema/v2_1"
+mkdir -p "ASN.1 schema/v2_1"
+
+asn1extractor/asn1extractor.sh MP_DispatcherHeaderModule \
+  com.saicmotor.telematics.tsgp.otaadapter.mp.v2_1.entity.dispatcher.MP_DispatcherHeader \
+  > "ASN.1 schema/v2_1/MP_DispatcherHeader.asn1"
+
+asn1extractor/asn1extractor.sh MP_DispatcherBodyModule \
+  com.saicmotor.telematics.tsgp.otaadapter.mp.v2_1.entity.dispatcher.MP_DispatcherBody \
+  > "ASN.1 schema/v2_1/MP_DispatcherBody.asn1"
+
+asn1extractor/asn1extractor.sh ApplicationDataModule \
+  com.saicmotor.telematics.tsgp.otaadapter.mp.v2_1.entity.alarm.MP_SecurityAlarmResp \
+  com.saicmotor.telematics.tsgp.otaadapter.mp.v2_1.entity.vehicle.OTA_RVMVehicleStatusReq \
+  com.saicmotor.telematics.tsgp.otaadapter.mp.v2_1.entity.vehicle.OTA_RVMVehicleStatusResp25857 \
+           > "ASN.1 schema/v2_1/ApplicationData.asn1"
+
