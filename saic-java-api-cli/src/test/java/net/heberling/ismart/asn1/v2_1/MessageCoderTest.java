bc128345d5302b1ed7714311901777fe2e84a7a4
Markus Heberling <markus@heberling.net>
2022-11-20 19:09:01 +0100

    added message type 510_25857 VehicleControlCommand

--- saic-java-api-cli/src/test/java/net/heberling/ismart/asn1/v2_1/MessageCoderTest.java
+++ saic-java-api-cli/src/test/java/net/heberling/ismart/asn1/v2_1/MessageCoderTest.java
@@ -2,6 +2,8 @@ package net.heberling.ismart.asn1.v2_1;
 
 import net.heberling.ismart.asn1.AbstractMessageCoderTest;
 import net.heberling.ismart.asn1.v2_1.entity.MP_SecurityAlarmResp;
+import net.heberling.ismart.asn1.v2_1.entity.OTA_RVCReq;
+import net.heberling.ismart.asn1.v2_1.entity.OTA_RVCStatus25857;
 import net.heberling.ismart.asn1.v2_1.entity.OTA_RVMVehicleStatusReq;
 import net.heberling.ismart.asn1.v2_1.entity.OTA_RVMVehicleStatusResp25857;
 import org.bn.coders.IASN1PreparedElement;
@@ -42,5 +44,16 @@ class MessageCoderTest extends AbstractMessageCoderTest {
     void decodeEncodeResponse_511() {
 
         decodeEncode("511_25857_response", new MessageCoder<>(OTA_RVMVehicleStatusResp25857.class));
+    }
+
+    @Test
+    void decodeEncodeRequest_510() {
+        // https://tap-eu.soimt.com/TAP.Web/ota.mpv21
+        decodeEncode("510_25857_request", new MessageCoder<>(OTA_RVCReq.class));
+    }
+
+    @Test
+    void decodeEncodeResponse_510() {
+        decodeEncode("510_25857_response", new MessageCoder<>(OTA_RVCStatus25857.class));
     }
 }
