44c6a4b79506ef9234e7926744a42d050c413d60
Markus Heberling <markus@heberling.net>
2022-12-13 10:21:53 +0100

    Correctly serialize @ASN1SizeConstraint strings

--- saic-java-api/src/main/java/net/heberling/ismart/asn1/AbstractMessageCoder.java
+++ saic-java-api/src/main/java/net/heberling/ismart/asn1/AbstractMessageCoder.java
@@ -2,12 +2,17 @@ package net.heberling.ismart.asn1;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import org.bn.annotations.constraints.ASN1SizeConstraint;
+import org.bn.annotations.constraints.ASN1ValueRangeConstraint;
 import org.bn.coders.CoderUtils;
 import org.bn.coders.DecodedObject;
 import org.bn.coders.ElementInfo;
 import org.bn.coders.IASN1PreparedElement;
 import org.bn.coders.per.PERUnalignedDecoder;
 import org.bn.coders.per.PERUnalignedEncoder;
+import org.bn.metadata.constraints.ASN1SizeConstraintMetadata;
+import org.bn.metadata.constraints.ASN1ValueRangeConstraintMetadata;
+import org.bn.metadata.constraints.IASN1ConstraintMetadata;
 import org.bn.utils.BitArrayInputStream;
 import org.bn.utils.BitArrayOutputStream;
 
@@ -93,6 +98,55 @@ public abstract class AbstractMessageCoder<
                 }
             }
             return encodeLength;
+        }
+
+        public int encodeLength(int value, ElementInfo elementInfo, OutputStream stream)
+                throws Exception {
+            int resultSize = 0;
+            BitArrayOutputStream bitStream = (BitArrayOutputStream) stream;
+            CoderUtils.checkConstraints(value, elementInfo);
+            if (elementInfo.hasPreparedInfo()) {
+                if (elementInfo.getPreparedInfo().hasConstraint()) {
+                    IASN1ConstraintMetadata constraint =
+                            elementInfo.getPreparedInfo().getConstraint();
+                    if (constraint instanceof ASN1ValueRangeConstraintMetadata) {
+                        resultSize +=
+                                encodeConstraintLengthDeterminant(
+                                        value,
+                                        (int)
+                                                ((ASN1ValueRangeConstraintMetadata) constraint)
+                                                        .getMin(),
+                                        (int)
+                                                ((ASN1ValueRangeConstraintMetadata) constraint)
+                                                        .getMax(),
+                                        bitStream);
+                    } else if (constraint instanceof ASN1SizeConstraintMetadata) {
+                        // exact size, don't need to encode it in the stream
+                        resultSize += (int) ((ASN1SizeConstraintMetadata) constraint).getMax();
+                    }
+                } else {
+                    resultSize += encodeLengthDeterminant(value, bitStream);
+                }
+            } else if (elementInfo
+                    .getAnnotatedClass()
+                    .isAnnotationPresent(ASN1ValueRangeConstraint.class)) {
+                ASN1ValueRangeConstraint constraint =
+                        elementInfo
+                                .getAnnotatedClass()
+                                .getAnnotation(ASN1ValueRangeConstraint.class);
+                resultSize +=
+                        encodeConstraintLengthDeterminant(
+                                value, (int) constraint.min(), (int) constraint.max(), bitStream);
+            } else if (elementInfo
+                    .getAnnotatedClass()
+                    .isAnnotationPresent(ASN1SizeConstraint.class)) {
+                ASN1SizeConstraint constraint =
+                        elementInfo.getAnnotatedClass().getAnnotation(ASN1SizeConstraint.class);
+                resultSize += (int) constraint.max();
+            } else {
+                resultSize += encodeLengthDeterminant(value, bitStream);
+            }
+            return resultSize;
         }
     }
 
