92a0f6e3ade5fefcf3aa27dbd08e20a04cb51a3b
Markus Heberling <markus@heberling.net>
2023-03-29 22:38:08 +0200

    Project moved to https://github.com/SAIC-iSmart-API

--- asn1extractor/asn1extractor.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-java -cp ${SCRIPT_DIR}/classes-dex2jar.jar:${SCRIPT_DIR}/classes2-dex2jar.jar:${SCRIPT_DIR}/target/asn1extractor-0.0.0-SNAPSHOT.jar net.heberling.ismart.reverseengineering.asn1extractor.ASN1Extractor "$@"
